4b1d82517622408b89c95405769c8500b9d6c939
Paul Abespy <paul.albespy@toulouse.miage.fr>
2018-05-23 17:32:32 +0200

    nanana

--- appliCliente/appliAngular/app.js
+++ appliCliente/appliAngular/app.js
@@ -1,7 +1,6 @@
-angular.module('miageIn', ['ui.router', 'ngResource'])
 var appMI = angular.module('miageIn', ['ui.router', 'ngResource', 'miageIn'])
         .constant('config', {
-          dataPath: 'http://localhost:3000/'
+			dataPath: 'http://localhost:3000/'
         })
         .config(['$qProvider', function ($qProvider) {
             $qProvider.errorOnUnhandledRejections(false);
@@ -9,10 +8,8 @@ var appMI = angular.module('miageIn', ['ui.router', 'ngResource', 'miageIn'])
         .config(["$stateProvider", "$urlServiceProvider", function ($stateProvider, $urlServiceProvider) {
                 $stateProvider
                         .state('renseignement', {
-                            url: '/renseignement-utilisateur',
                             url: '/utilisateur',
                             templateUrl: 'vues/renseignement.html',
-                            //controller: 'renseignementCtrl'
                             controller: 'renseignementCtrl'
                         })
                         .state('evenements', {
@@ -20,22 +17,15 @@ var appMI = angular.module('miageIn', ['ui.router', 'ngResource', 'miageIn'])
                             templateUrl: 'vues/evenements.html',
                             controller: 'CtrlEvenement'
                         })
-						.state('statistiques', {
-						            .state('statistiques', {
+						    .state('statistiques', {
                             url: '/statistiques',
                             templateUrl: 'vues/statistiques.html',
                             //controller: 'statistiquesCtrl'
                         })
-						.state('evenementsCrea', {
-						            .state('evenementsCrea', {
+						    .state('evenementsCrea', {
                             url: '/creation-evenements',
                             templateUrl: 'vues/evenementsCrea.html',
-                            //controller: 'evenementsCreaCtrl'
                             controller: 'evenementsCreaCtrl'
                         })
-						$urlServiceProvider.rules.otherwise({state: 'renseignement'});
 					//	$urlServiceProvider.rules.otherwise({state: 'renseignement'});
             }]);
-			
-
-
